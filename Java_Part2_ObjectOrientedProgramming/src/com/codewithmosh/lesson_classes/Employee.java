b9adfdfd00a6848e30793c43f1a201787d13bd58
jriboli <jmriboli@yahoo.com>
2023-09-06 13:05:28 -0700

    New Part2 folder created

---
+++ Java_Part2_ObjectOrientedProgramming/src/com/codewithmosh/lesson_classes/Employee.java
@@ -0,0 +1,29 @@
+package com.codewithmosh.lesson_classes;
+
+public class Employee {
+    private int baseSalary;
+    private int hourlyRate;
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (hourlyRate * extraHours);
+    }
+
+    public void setBaseSalary(int baseSalary){
+        if(baseSalary <= 0)
+            throw new IllegalArgumentException("Base Salary needs to be greater than zero.");
+        this.baseSalary = baseSalary;
+    }
+
+    public int getBaseSalary(){
+        return this.baseSalary;
+    }
+
+    public void setHourlyRate(int hourlyRate){
+        if(hourlyRate <= 0)
+            throw new IllegalArgumentException("Hourly Rate needs to be greater than zero.");
+        this.hourlyRate = hourlyRate;
+    }
+    public int getHourlyRate(){
+        return hourlyRate;
+    }
+}
---
+++ Java_Part2_ObjectOrientedProgramming/src/com/codewithmosh/lesson_refactoring/Main.java
@@ -0,0 +1,4 @@
+package com.codewithmosh.lesson_refactoring;
+
+public class Main {
+}
